89776f38bf0ec251e8a8b93753a099439ec6facf
KURn3SAN <49362763+kurn3san@users.noreply.github.com>
2023-02-26 16:22:15 +0300

    Update App.js

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,7 +1,21 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const App1 = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
 
-export default App
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+}
+const App2=()=>{
+  const App2 = () => {
+    
+  }
+}
+export default App1
