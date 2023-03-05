ace335fb719330b96529f9a4ce683927dcbe1006
KURn3SAN <49362763+kurn3san@users.noreply.github.com>
2023-03-06 00:39:00 +0300

    step 1.5

--- part1/applicatino1/src/App.js
+++ part1/applicatino1/src/App.js
@@ -186,5 +186,63 @@ const Total=(props) => {
       <Total TotalThing={parts}/>      
   </>)
 }
+const App15 = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  const Header=(props)=>{
+    console.log("header called")
+    console.log(props.course.name)
+    return(<>
+      <h1>{course.name}</h1>
+    </>)
+  }
+  const Part=(props)=>{
+    console.log("a part was called...")
+    return(
+    <p>{props.part.name} {props.part.exercises}</p>
+    )
+  }
+  const Content=(props)=>{
+    console.log("content called..")
+    console.log(props.parts)
+    return(<>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
+      
+    </>)
+  }
+  const Total=(props)=>{
+    console.log("Total was called")
+    return(
+      <>
+      <p>Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
+      </>
+    )
+  }
 
-export default App14
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App15
